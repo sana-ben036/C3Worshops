c9aedc6317e195cf387f1c0dc838511581053b76
SANA BEN <sana.bengannoune@gmail.com>
2020-03-10 01:03:03 +0100

    test 1

---
+++ tp.js
@@ -0,0 +1,42 @@
+// question 1 et 2 ...........................................
+
+// var bookName = "Great";
+// var favoriteBook = [" "];
+
+// function addFavoriteBook(bookName){
+//     if(bookName !== "Great"){
+//         favoriteBook.push("Great");
+//     }else{
+//         alert("exist");
+//         } 
+
+// }addFavoriteBook();
+// console.log(favoriteBook);
+
+
+
+// question 3 et 4 ..............................................   
+
+// var favoriteBook = ["k","m","c"];
+
+// function printFavoriteBooks(){
+//     var a = favoriteBook.length;
+//     console.log("livres"+" "+"favoris:"+ a);
+
+
+// }printFavoriteBooks();
+
+// question 5 ..............................................  
+
+var favoriteBook = ["k","m","c"];
+var a = favoriteBook.length;
+var i;
+
+function printFavoriteBooks(){
+    
+    for(var i=0; i< a ; i++){
+       console.log(bookName);
+
+    }
+}printFavoriteBooks();
+
