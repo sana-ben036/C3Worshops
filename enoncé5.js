ab7c57dd915688c9bc977ea8a8cb1a13f33d6d5e
SANA BEN <sana.bengannoune@gmail.com>
2020-03-20 15:56:41 +0100

    Update enoncé5.js

--- enoncé5.js
+++ enoncé5.js
@@ -4,10 +4,15 @@
 -tableau a deux dimension??
 
 */
+var albums=[id, prop, valeur];
+albums[id]= 2548;
+albums[prop]='tracks';
+albums[valeur]='Addicted to Love'
+
 
 
 function updateRecords(id, prop, valeur) {
-    var albums=[];
+    
     if (prop === "tracks" && (!(albums[id,prop.tracks]))) {
         albums[id,prop] = [];
     }
